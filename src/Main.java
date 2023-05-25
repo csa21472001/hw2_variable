ca611bb0ab28ee8f03623b2430454f46edc969cc
csa21472001 <21461901as@gmail.com>
2023-05-25 15:31:01 +0300

    ДЗ.Переменные. Мастер-ветка.

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
     /*
         task1();
         task2();
-        task3();//
+        task3();
         task4();
-            task5();
-            task6();
-            task7();
-            task8();
+        task5();
+        task6();
+        task7();
+        task8();
 
     }
 
