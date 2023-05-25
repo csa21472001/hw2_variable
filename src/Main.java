98f91fdc42fc1eda3207e010d8383b0c72f4e5ec
csa21472001 <21461901as@gmail.com>
2023-05-25 13:39:48 +0300

    ДЗ.Переменные. Мастер ветка в комментарии

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("ДЗ Переменные");
-        }  }/*         task1();
+        }  }/*         task1();4
         task2();
         task3();//hhlkjjhliuh
         task4();
