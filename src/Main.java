e08d1c012e7f9502822be9b4a97b831f18c49442
csa21472001 <21461901as@gmail.com>
2023-05-25 11:48:51 +0300

    ДЗ.Переменные

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,91 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-        }
+        task1();
+        task2();
+        task3();
+        task4();
+            task5();
+            task6();
+            task7();
+            task8();
+
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + (dog+4));
+        System.out.println("cat = " + (cat+4));
+        System.out.println("paper = " +  (paper+4));
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + (dog+4-3.5));
+        System.out.println("cat = " + (cat+4-1.6));
+        System.out.println("paper = " +  (paper+4-7639));
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        var friend = 19;
+       /* var cat = 3.6;
+        var paper = 763789;*/
+        System.out.println(friend);
+        friend = friend*2;
+        System.out.println(friend);
+        friend =  friend/7;
+        System.out.println(friend);
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog =  frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var commonWeight = boxer1+boxer2;
+        System.out.println(commonWeight);
+        var weightDiff = boxer2 - boxer1;
+        System.out.println(weightDiff);
+    }
+    public static void task7() {
+        System.out.println("Задача 7");
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var weightDiff_1 = boxer2 - boxer1;
+        System.out.println(weightDiff_1);
+        var weightDiff_2 = boxer2%boxer1;
+        System.out.println(weightDiff_2);
+    }
+    public static void task8() {
+        System.out.println("Задача 8");
+        var workersAmount_1 = 640/8;
+        System.out.println("Всего работников в компании-"+workersAmount_1+" человек");
+        var workersAmount_2 = 80+94;
+        var workingHours = workersAmount_2 * 8;
+        System.out.println("Если в компании работает " + workersAmount_2 + " человек, то всего " + workingHours + " часов работы может быть поделено между сотрудниками");
+    }
+}
